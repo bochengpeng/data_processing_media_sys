467355f4999852c743d1271d41d18466aed936ab
traanf-thuw <thu.tran1@student.nhlstenden.com>
2025-01-19 11:07:01 +0100

    delete unnecessary folder

--- MediaSystem/src/main/java/org/example/moviesystem/repository/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.moviesystem.repository;
-
-import org.example.moviesystem.table.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, String>
-{
-}
-
